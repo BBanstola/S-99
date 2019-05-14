23f61c1e850a68888d631b60b13260b17535a4ed
Bidur_Banstola <bidur.banstola@gmail.com>
2019-05-14 23:45:20 +0545

    Trees

---
+++ src/BinaryTrees/Symmetric.scala
@@ -0,0 +1,17 @@
+package BinaryTrees
+
+// P56 (**) Symmetric binary trees.
+//     Let us call a binary tree symmetric if you can draw a vertical line
+//     through the root node and then the right subtree is the mirror image of
+//     the left subtree.  Add an isSymmetric method to the Tree class to check
+//     whether a given binary tree is symmetric.  Hint: Write an isMirrorOf
+//     method first to check whether one tree is the mirror image of another.
+//     We are only interested in the structure, not in the contents of the
+//     nodes.
+//
+//     scala> Node('a', Node('b'), Node('c')).isSymmetric
+//     res0: Boolean = true
+
+object Symmetric {
+
+}
